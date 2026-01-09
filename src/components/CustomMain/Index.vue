450230dc3d3063633593b136dd8b0def30e3fd0d
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2026-01-09 17:24:03 +0300

    fix CustomMain component

--- src/components/CustomMain/Index.vue
+++ src/components/CustomMain/Index.vue
@@ -2,10 +2,6 @@
 
 <template>
   <main class="main">
-    <div class="main__list">
-      <CustomMainListOfTabs />
-      <MainStone />
-    </div>
     <div class="main__content">
       <slot />
     </div>
@@ -21,16 +17,11 @@
   flex-direction: column;
   padding-bottom: 6px;
 
-  &__list {
-    flex-shrink: 0;
-  }
   &__content {
     background: var(--color-background--1);
     flex-grow: 1;
     overflow: auto;
-    padding: 3rem;
-    border-bottom-left-radius: var(--border-radius--2);
-    border-bottom-right-radius: var(--border-radius--2);
+    border-radius: var(--border-radius--2);
     display: flex;
     flex-direction: column;
     justify-content: start;
