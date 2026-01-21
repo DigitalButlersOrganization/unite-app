93c081fdfd25fb3068b8f5f20b1f1ff804f31ec3
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2026-01-21 12:00:54 +0300

    add new enums

---
+++ src/enums/milestone.ts
@@ -0,0 +1,16 @@
+export enum MILESTONE_TYPES {
+  FORM = 'form',
+  PAYMENT = 'payment',
+  REVIEW = 'review',
+  EVENT = 'event',
+}
+
+export enum MILESTONE_STATUSES {
+  DEFAULT = 'default',
+  ACTIVE = 'active',
+  COMPLETED = 'completed',
+  PENDING = 'pending',
+  REJECTED = 'rejected',
+  BASE = 'base',
+  DISABLED = 'disabled',
+}
