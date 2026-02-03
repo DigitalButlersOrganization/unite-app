f4fa6ab5f175a11430b8cda6126e3630e044d53e
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2026-02-03 12:09:42 +0300

    change IEventStep interface

--- src/types/event.d.ts
+++ src/types/event.d.ts
@@ -35,9 +35,8 @@ export interface IEventMenu {
 export interface IEventStep {
   milestone: IMilestone;
   status: string;
-  startedAt: string;
-  completedAt: string;
-  expiredAt: string;
+  timeRemaining: number;
+  lifeBar: number;
   payment: IMilestonePayment;
 }
 
